197bc8b2b616611b8b3e2bf7c73baa863cfafb49
Joe McGarry <joewmcgarry@gmail.com>
2022-05-04 10:18:42 +0100

    directory.rb can now save student names

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -25,6 +26,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -45,6 +48,18 @@ def input_students
     #get another name from the user
     name = gets.chomp
   end
+end
+
+def save_students
+  #Open the file for writing
+  file = File.open("students.csv", "w")
+  #Iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
